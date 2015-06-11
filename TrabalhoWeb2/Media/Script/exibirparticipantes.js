3fbf1cc434fc8331fa36a2335ee05d9d2de756a2
marcelohmdias <marcelohmdias@gmail.com>
2015-06-11 02:05:54 -0300

    Correções no JS, definição do layout das telas.

---
+++ TrabalhoWeb2/Media/Script/exibirparticipantes.js
@@ -0,0 +1,67 @@
+﻿$(document).ready(
+    function () {
+        $.getJSON(
+            '/Agenda/listarUsuario',
+                null,
+                function (data) {
+                    $('#lstUsuario').find('option').remove();
+                    $.each(
+                        data,
+                        function (index) {
+                            $('#lstUsuario').append('<option value="' + data[index].UsuarioId + '">' + data[index].Nome + '</option>');
+                        })
+
+                });
+
+        $.getJSON(
+            '/Agenda/retornaParticipantes',
+            { evento: $('#AgendaId').val() },
+            function (val) {
+                tag = '';
+                $.each(
+                    val,
+                    function (ind) {
+                        tag += '<div class="lista-participante">' + val[ind].Nome + '</div>';
+                    });
+                $("#tdBody").append(tag);
+            });
+
+        $('#btnAdicionar').click(
+            function () {
+                $('#carregando').html('Salvando Dados. Aguarde!');
+
+                $.post(
+                    '/Agenda/adicionarParticipante',
+                {
+                    IdNovoUsuario: $('#lstUsuario').val(),
+                    IdAgenda: $('#AgendaId').val(),
+                    IdUsuario: $('#UsuarioId').val(),
+                },
+                function () {
+                    $('#carregando').html('');
+                    alert('Participante adicionado com sucesso!');
+                    location.reload();
+                }
+                )
+            });
+
+        $('#btnRemover').click(
+            function () {
+                $('#carregando').html('Salvando Dados. Aguarde!');
+
+                $.post(
+                    '/Agenda/removerParticipante',
+                {
+                    IdNovoUsuario: $('#lstUsuario').val(),
+                    IdAgenda: $('#AgendaId').val(),
+                    IdUsuario: $('#UsuarioId').val(),
+                },
+                function () {
+                    $('#carregando').html('');
+                    alert('Participante removido com sucesso!');
+                    location.reload();
+                }
+                )
+            });
+
+    });
